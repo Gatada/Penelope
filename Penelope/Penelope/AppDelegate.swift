bb74bba2b1f25862b40bc6194e94a86f2523c628
Johan Basberg <johan.basberg@igt.com>
2018-01-29 14:55:04 +0000

    Initial Commit
    Created Main Window layout.

---
+++ Penelope/Penelope/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Penelope
+//
+//  Created by Basberg, Johan on 29/01/2018.
+//  Copyright © 2018 Basberg, Johan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Penelope/Penelope/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Penelope
+//
+//  Created by Basberg, Johan on 29/01/2018.
+//  Copyright © 2018 Basberg, Johan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
